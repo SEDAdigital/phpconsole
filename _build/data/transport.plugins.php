cb03c9566b5124b5c949f2e8b1984d224e82ba96
Christian Seel <cs@chsmedien.de>
2016-01-20 10:11:41 +0100

    Added first files for build script

---
+++ _build/data/transport.plugins.php
@@ -0,0 +1,26 @@
+<?php
+$plugins = array();
+
+/* create the plugin object */
+$plugins[0] = $modx->newObject('modPlugin');
+$plugins[0]->set('id',1);
+$plugins[0]->set('name','phpconsole');
+$plugins[0]->set('description','The plugin that sends logging messages to phpconsole after send the client response');
+$plugins[0]->set('plugincode', getSnippetContent($sources['plugins'] . 'phpconsole.plugin.php'));
+$plugins[0]->set('category', 0);
+
+$events = array();
+$events['OnWebPageComplete'] = $modx->newObject('modPluginEvent');
+$events['OnWebPageComplete']->fromArray(array(
+    'event' => 'OnWebPageComplete',
+    'priority' => 0,
+    'propertyset' => 0
+),'',true,true);
+
+$plugins[0]->addMany($events);
+
+$modx->log(xPDO::LOG_LEVEL_INFO,'Packaged in '.count($events).' Plugin Events.'); flush();
+
+unset($events);
+return $plugins;
+?>
